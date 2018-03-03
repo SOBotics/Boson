f5b29369c53ce3a0e656e4d61c789ad66df39d12
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-03-03 01:45:33 -0800

    Added a new endpoint

--- src/main/java/org.sobotics.boson/framework/services/data/ApiService.java
+++ src/main/java/org.sobotics.boson/framework/services/data/ApiService.java
@@ -1,13 +1,12 @@
 package org.sobotics.boson.framework.services.data;
 
 import org.sobotics.boson.framework.exceptions.StackExchangeApiException;
-import org.sobotics.boson.framework.exceptions.TypeSizeExceededException;
 import org.sobotics.boson.framework.model.stackexchange.Answer;
 import org.sobotics.boson.framework.model.stackexchange.Comment;
 import org.sobotics.boson.framework.model.stackexchange.Question;
+import org.sobotics.boson.framework.model.stackexchange.api.AnswerSorting;
 import org.sobotics.boson.framework.model.stackexchange.api.CommentSorting;
 import org.sobotics.boson.framework.model.stackexchange.api.PostOrdering;
-import org.sobotics.boson.framework.model.stackexchange.api.AnswerSorting;
 import org.sobotics.boson.framework.model.stackexchange.api.QuestionSorting;
 
 import java.io.IOException;
@@ -15,6 +14,10 @@ import java.time.Instant;
 import java.util.List;
 
 public abstract class ApiService {
+
+    public List<Answer> getAnswers(String site) throws IOException {
+        return getAnswers(site, 1, 30, null, null, PostOrdering.DESC, AnswerSorting.ACTIVITY);
+    }
 
     public List<Answer> getAnswers(String site, int page, int pageSize, Instant fromDate) throws IOException {
         return getAnswers(site, page, pageSize, fromDate, Instant.now(), PostOrdering.DESC, AnswerSorting.ACTIVITY);
