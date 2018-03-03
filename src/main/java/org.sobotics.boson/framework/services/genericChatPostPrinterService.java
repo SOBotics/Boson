081e352d406c7589636bc8aec866335d02625629
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-03-02 23:27:10 -0800

    Added a generic post printer service

---
+++ src/main/java/org.sobotics.boson/framework/services/genericChatPostPrinterService.java
@@ -0,0 +1,23 @@
+package org.sobotics.boson.framework.services;
+
+import org.sobotics.boson.framework.model.stackexchange.Answer;
+import org.sobotics.boson.framework.model.stackexchange.Comment;
+import org.sobotics.boson.framework.model.stackexchange.Question;
+
+public class genericChatPostPrinterService {
+    private String sitename;
+
+    public genericChatPostPrinterService(String sitename) {
+        this.sitename = sitename;
+    }
+
+    public String print(Comment comment){
+        return "[ [Boson](https://git.io/vA9TM) ] New comment posted on "+ sitename+ " " + comment.getLink();
+    }
+    public String print(Answer answer){
+        return "[ [Boson](https://git.io/vA9TM) ] New answer posted on "+ sitename+ " " + answer.getLink();
+    }
+    public String print(Question question){
+        return "[ [Boson](https://git.io/vA9TM) ] New question posted on "+ sitename+ " " + question.getLink();
+    }
+}
