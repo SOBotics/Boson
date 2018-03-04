aea550bd1a5cf87174241e7167ca14e24c5758bb
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-03-03 21:29:09 -0800

    Removed the superuser bot; Increased frequency to 6 mins

--- src/main/java/org.sobotics.boson/Boson.java
+++ src/main/java/org.sobotics.boson/Boson.java
@@ -5,7 +5,6 @@ import fr.tunaki.stackoverflow.chat.Room;
 import fr.tunaki.stackoverflow.chat.StackExchangeClient;
 import org.sobotics.boson.framework.services.PropertyService;
 import org.sobotics.boson.sample.AnswerPrinterBot;
-import org.sobotics.boson.sample.QuestionPrinterBot;
 
 public class Boson{
 
@@ -13,9 +12,7 @@ public class Boson{
         PropertyService propertyService = new PropertyService();
         StackExchangeClient client = new StackExchangeClient(propertyService.getProperty("email"), propertyService.getProperty("password"));
         Room room = client.joinRoom(ChatHost.STACK_EXCHANGE, 59667);
-        AnswerPrinterBot interpersonalAnswerBot = new AnswerPrinterBot(room, "interpersonal", 120);
-        QuestionPrinterBot superuserQuestionBot = new QuestionPrinterBot(room, "superuser", 120);
+        AnswerPrinterBot interpersonalAnswerBot = new AnswerPrinterBot(room, "interpersonal", 660);
         interpersonalAnswerBot.start();
-        superuserQuestionBot.start();
     }
 }
