789a62fcf3de0a3ee2fc2a1ae7fcde41ba0a5862
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-07-15 01:20:39 -0700

    Added a new UserID filter as requested in TL

---
+++ src/main/java/org.sobotics.boson/framework/services/chat/filters/UserIdFilter.java
@@ -0,0 +1,19 @@
+package org.sobotics.boson.framework.services.chat.filters;
+
+import org.sobotics.boson.framework.model.stackexchange.Content;
+
+public class UserIdFilter implements Filter<Content> {
+    private int userId;
+
+    // TODO REMOVE THIS IN PRODUCTION!!!!!!!!!!!!!
+
+    public UserIdFilter(int userId){
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean filter(Content data) {
+        return data.getOwner().getUserId() == userId;
+
+    }
+}
