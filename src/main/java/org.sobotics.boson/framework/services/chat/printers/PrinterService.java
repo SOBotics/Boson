34a3ebb171a759cd03b5e93795e747d21b6e6cea
Bhargav-Rao <bhargav.rao448@gmail.com>
2019-03-03 19:27:37 -0800

    Added support for Dashboards.

--- src/main/java/org.sobotics.boson/framework/services/chat/printers/PrinterService.java
+++ src/main/java/org.sobotics.boson/framework/services/chat/printers/PrinterService.java
@@ -2,4 +2,6 @@ package org.sobotics.boson.framework.services.chat.printers;
 
 public interface PrinterService <T>{
     String print(T object);
+    String print(T object, String dashboard);
+    String print(T object, String dashboard, String intro);
 }
